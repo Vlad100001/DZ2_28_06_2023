54c9de998af904a9c8a2cbff5a843bc5bd93face
Vlad100001 <132137476+Vlad100001@users.noreply.github.com>
2023-05-28 13:00:59 +0300

    Update Program.cs

--- ex13/Program.cs
+++ ex13/Program.cs
@@ -1,24 +1,16 @@
-﻿
-        Console.Clear();
+﻿  Console.Clear();
         Console.Write("Введите число: ");
         int n = int.Parse(Console.ReadLine()!);
-        int i = 1;
-        int k = n;
-        int x = 10;
-        int j = 1;
 
-        if (n < 99 && n > -99)
-             Console.Write("третьей цифры нет");
-           
-        else  
-        {{for (int k = n / 10; k <= 0; i++);}
-        Console.Write($"{i} ");
-        i=i-2;
-        
-       for (x= x*10; i == j; j++);
-                n=n/x;
-                n = n % 10;
-                 Console.Write($"{j} ");
-            Console.Write($"{n} ");}
+
+        if (n > 99 && n < -99) 
+       {
+          while (n>999)
+             n=n/10;
+             return n=n%10;
+         Console.Write(n);
+         }
+        else 
+        Console.Write("третьей цифры нет");
        
     
