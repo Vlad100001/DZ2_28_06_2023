1f3c70e260e7dd83437faf47d0f364c5f7dc0789
Vlad100001 <132137476+Vlad100001@users.noreply.github.com>
2023-05-28 12:41:16 +0300

    Add files via upload

---
+++ ex13/Program.cs
@@ -0,0 +1,24 @@
+﻿
+        Console.Clear();
+        Console.Write("Введите число: ");
+        int n = int.Parse(Console.ReadLine()!);
+        int i = 1;
+        int k = n;
+        int x = 10;
+        int j = 1;
+
+        if (n < 99 && n > -99)
+             Console.Write("третьей цифры нет");
+           
+        else  
+        {{for (int k = n / 10; k <= 0; i++);}
+        Console.Write($"{i} ");
+        i=i-2;
+        
+       for (x= x*10; i == j; j++);
+                n=n/x;
+                n = n % 10;
+                 Console.Write($"{j} ");
+            Console.Write($"{n} ");}
+       
+    
