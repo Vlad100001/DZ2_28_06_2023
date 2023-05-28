565c0ffa36daacdc843b0efd32c21b8d715577b3
Vlad100001 <132137476+Vlad100001@users.noreply.github.com>
2023-05-28 12:37:34 +0300

    Add files via upload

---
+++ ex10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+if (n > 99 && n < 1000)
+{n = n / 10;
+n= n % 10;
+Console.WriteLine(n);}
+else
+Console.WriteLine("ошибка");
+    
