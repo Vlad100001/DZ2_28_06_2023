24c0cca6a744b1733dfa3ad878bf8c373b0afaca
Vlad100001 <132137476+Vlad100001@users.noreply.github.com>
2023-05-28 12:42:39 +0300

    Add files via upload

---
+++ ex15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблиись!\nВведите номер дня недели: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+if (n < 1 || n > 5)
+    Console.WriteLine("да");
+else 
+    Console.WriteLine("нет");
